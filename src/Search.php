ecc89b7ef58e92724b13015dfd2f8b4f9c8181a0
Ismael Barroso <mael-sf@hotmail.com>
2022-08-06 22:45:14 -0300

    utilizando phpCodeSniffer

--- src/Search.php
+++ src/Search.php
@@ -4,15 +4,15 @@ namespace Ismael\Cepdigital;
 
 class Search
 {
-  private $url = 'https://viacep.com.br/ws/';
+    private $url = 'https://viacep.com.br/ws/';
 
-  public function getAdressFromZipcode(string $zipCode): array
-  {
-    $zipCode =  preg_replace('/[^0-9]/im', '', $zipCode);
+    public function getAdressFromZipcode(string $zipCode): array
+    {
+        $zipCode =  preg_replace('/[^0-9]/im', '', $zipCode);
 
-    $get = file_get_contents($this->url . $zipCode . "/json");
+        $get = file_get_contents($this->url . $zipCode . "/json");
 
-    print_r($get);
-    return (array) json_decode($get);
-  }
+        print_r($get);
+        return (array) json_decode($get);
+    }
 }
